838c1a83555c76f9998a27c013b622d20747b473
Hugo Berleur <h.berleur@synelia.fr>
2022-03-23 17:02:09 +0100

    Add footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php 
+
+    echo "<footer id=\"footer\">";
+    echo "<div id=\"container\">";
+    echo "<p>AS BEUVRY LA FORÊT - &copy; COPYRIGHT 2022, Tous droits réservés.</p>";
+    echo "</div>";
+    echo "</footer>";
+
+?>
