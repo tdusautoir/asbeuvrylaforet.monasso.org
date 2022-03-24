c14180c4795a7c359911c53b0a055f7698ce0ed6
WpFeltac <hberleur@gmail.com>
2022-03-24 10:40:36 +0100

    Update footer.php
    Plus de balises php ni d'echo, contient directement le code html

--- footer.php
+++ footer.php
@@ -1,9 +1,17 @@
-<?php 
-
-    echo "<footer id=\"footer\">";
-    echo "<div id=\"container\">";
-    echo "<p>AS BEUVRY LA FORÊT - &copy; COPYRIGHT 2022, Tous droits réservés.</p>";
-    echo "</div>";
-    echo "</footer>";
-
-?>
+<footer class="footer">
+    <ul>
+        <li class="liste">
+           AS BEUVRY LA FORÊT - &copy; COPYRIGHT 2022, Tous droits réservés.
+        </li>
+        <li class="liste">
+           <a href="">
+               <img src="media/Facebook.png" alt="facebook" class="icones">
+            </a>
+        </li>
+        <li class="liste">
+            <a href="#">
+                <img src="media/Instagram.png" alt="instagram" class="icones">
+            </a>
+        </li>
+    </ul>
+</footer>    
