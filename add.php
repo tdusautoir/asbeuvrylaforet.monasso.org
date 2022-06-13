c877b55b0245829e62750674896a59a50ca15509
Achille <davidachille18@gmail.com>
2022-06-13 16:27:54 +0200

    page ajout licenciés

--- add.php
+++ add.php
@@ -14,6 +14,7 @@ if (isset($_GET['action']) && !empty($_GET['action']) && $_GET['action'] == "log
 <head> <?php require("head.php"); ?>
   <link rel="stylesheet" href="./public/css/login.css">
   <link rel="stylesheet" href="./public/css/style.css">
+  <link rel="stylesheet" href="dev.css">
   <title>Espace Admin - A.S. BEUVRY LA FORÊT</title>
 </head>
 
@@ -22,71 +23,17 @@ if (isset($_GET['action']) && !empty($_GET['action']) && $_GET['action'] == "log
     <?php include('header.php'); ?>
     <div class="container">
       <div class="container-content">
-        <div class="welcome-admin">
-          <h1>Bonjour <?= htmlspecialchars($_SESSION['prenom']); ?>,
-            <p>Tu es sur l'espace d'administration</p>
-          </h1>
-        </div>
-        <div class="welcome-separator"></div>
-        <div class="admin-panel">
-          <a>
-            <i class="fa fa-plus"></i> 
-            <p>Ajouter un &eacute;ducateur</p>
-          </a>
-          <a href="add.php">
-            <i class="fa fa-user-plus"></i> 
-            <p>Ajouter un licenci&eacute;</p>
-          </a>
-          <a>
-            <!-- <i class="fa fa-bar-chart"></i>  -->
-            <p>Statistiques de la saison</p>
-          </a>
-          <a>
-            <i class="fa fa-cogs"></i> 
-            <p>Mon compte</p>
-          </a>
-          <a>
-            <i class="fa fa-user"></i> 
-            <p>Gestion des &eacute;ducateurs</p>
-          </a>
-          <a>
-            <i class="fa fa-users"></i> 
-            <p>Gestion des licenci&eacute;s</p>
-          </a>
-          <a>
-            <i class="fa fa-euro-sign"></i> 
-            <p>Suivi des cotisations</p>
-          </a>
-          <a>
-            <i class="fa fa-file-invoice"></i> 
-            <p>G&eacute;n&eacute;rer une attestation</p>
-          </a>
-        </div>
-        <div class="admin-panel-separator"></div>
-        <div class="li-admin">
-          <h2>
-            Derniers liscenciés ajoutés :
-          </h2>
-          <?php
-          $req = $db->prepare("SELECT categorie.nomCategorie, licencie.prenom, licencie.nom, licencie.USRCRE  FROM `licencie` INNER JOIN categorie ON licencie.idCategorie = categorie.idCategorie ORDER BY licencie.DCRE DESC LIMIT 10"); //Derniers licenciés ajoutés classé par date croissant et limités à 10. 
-          $req->execute();
-          $rowCount = $req->rowCount();
-          if ($rowCount > 0) : //si on trouve des licenciés ajoutés on affiche la liste de la requete.
-          ?>
-            <ul>
-              <?php while ($LIC = $req->fetch(PDO::FETCH_ASSOC)) : ?>
-                <li>
-                  <p><?= $LIC["nomCategorie"] ?> - <span><?= $LIC["prenom"] . " " . strtoupper($LIC["nom"]) ?></span>
-                    <?php if (isset($LIC["USRCRE"])) : ?>par <span><?= ($LIC["USRCRE"]) ?> </span></p> <?php endif; ?>
-                </li>
-              <?php endwhile; ?>
-            </ul>
-          <?php else : ?>
-            <p> Aucun licencié n'a encore été crée </p>
-          <?php endif; ?>
+        <div class="add-panel">
+            <h1>
+                Ajouter un licencié
+            </h1>
+            <div class="mail-add">
+                <!-- <input type="text" class="Nom">
+                <input type="text" class="Prenom"> -->
+            </div>
         </div>
         <div class="deconnect">
-          <a href="index.php?action=logout" class="deco-button">Deconnexion</a>
+          <a href="index.php" class="deco-button">Retour</a>
         </div>
       </div>
     </div>
