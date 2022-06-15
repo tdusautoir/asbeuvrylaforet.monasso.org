bd741438570e23367f195ccf07d0f2172a795f91
Thibaut Dusautoir <tdusautoir@myges.fr>
2022-06-15 13:48:15 +0200

    fix deletion

--- functions/licencie-delete.php
+++ functions/licencie-delete.php
@@ -9,16 +9,27 @@ require_once("../db.php");
 if (isset($_GET["idLicencie"]) && !empty($_GET["idLicencie"])) {
     $idLicencie = $_GET["idLicencie"];
     if (filter_var($idLicencie, FILTER_VALIDATE_INT)) {
-        $req = $db->prepare("UPDATE licencie SET licencie.COSU = 1 WHERE `idLicencie` = ?;");
+        $req = $db->prepare("BEGIN; UPDATE licencie SET licencie.COSU = 1 WHERE licencie.idLicencie = ?; UPDATE cotis SET cotis.COSU = 1 WHERE cotis.idLicencie = ?; COMMIT;");
         $req->bindValue(1, $idLicencie, PDO::PARAM_INT);
+        $req->bindValue(2, $idLicencie, PDO::PARAM_INT);
         $result = $req->execute();
 
         if ($result) {
             create_flash_message("delete_success", "Le licencié a bien été supprimé.", FLASH_SUCCESS);
             header("location: ../licencies.php");
+            exit();
         } else {
             create_flash_message("delete_error", "Une erreur est survenue. Veuillez réessayer.", FLASH_ERROR);
             header("location: ../licencies.php");
+            exit();
         }
+    } else {
+        create_flash_message("delete_error", "Une erreur est survenue. Veuillez réessayer.", FLASH_ERROR);
+        header("location: ../licencies.php");
+        exit();
     }
+} else {
+    create_flash_message("delete_error", "Une erreur est survenue. Veuillez réessayer.", FLASH_ERROR);
+    header("location: ../licencies.php");
+    exit();
 }
