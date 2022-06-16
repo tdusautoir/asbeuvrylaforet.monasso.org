55e8f739da2656b01f2c01118ea73d2be40375e2
WpFeltac <hberleur@gmail.com>
2022-06-16 10:31:14 +0200

    Revert "display name when delete"
    This reverts commit 9c0fe55083992a26d85d26aad6c7fb1bf699fdbf.

--- functions/licencie-delete.php
+++ functions/licencie-delete.php
@@ -9,17 +9,13 @@ require_once("../db.php");
 if (isset($_GET["idLicencie"]) && !empty($_GET["idLicencie"])) {
     $idLicencie = $_GET["idLicencie"];
     if (filter_var($idLicencie, FILTER_VALIDATE_INT)) {
-        $info = $db->prepare("SELECT licencie.prenom FROM licencie WHERE licencie.idLicencie = $idLicencie");
         $req = $db->prepare("BEGIN; UPDATE licencie SET licencie.COSU = 1 WHERE licencie.idLicencie = ?; UPDATE cotis SET cotis.COSU = 1 WHERE cotis.idLicencie = ?; COMMIT;");
         $req->bindValue(1, $idLicencie, PDO::PARAM_INT);
         $req->bindValue(2, $idLicencie, PDO::PARAM_INT);
-        $info->execute();
         $result = $req->execute();
-        $getinfo = $info->fetch(PDO::FETCH_ASSOC);
-        $firstname_licencie = $getinfo["prenom"];
 
         if ($result) {
-            create_flash_message("delete_success", "Le licencié « $firstname_licencie » a bien été supprimé.", FLASH_SUCCESS);
+            create_flash_message("delete_success", "Le licencié a bien été supprimé.", FLASH_SUCCESS);
             header("location: ../licencies.php");
             exit();
         } else {
