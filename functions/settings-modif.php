f46d5352f8aa92ab999c57517f27bfab7cae169b
Thibaut Dusautoir <tdusautoir@myges.fr>
2022-07-06 14:12:55 +0200

    logo to 200x200

--- functions/settings-modif.php
+++ functions/settings-modif.php
@@ -36,8 +36,8 @@ if (is_logged()) {
                                 $image_width = $sourceProperties[0]; //get image width
                                 $image_height = $sourceProperties[1]; //get image height
                                 $imageType = $sourceProperties[2]; //get image type
-                                $newImage_width = 100;
-                                $newImage_height = 100;
+                                $newImage_width = 200;
+                                $newImage_height = 200;
 
                                 switch ($imageType) {
 
