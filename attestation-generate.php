0835481fc50e157a46248017d56053089f0f1420
jderache <derachejulien@orange.fr>
2022-07-10 17:51:48 +0200

    modif pdf

--- attestation-generate.php
+++ attestation-generate.php
@@ -15,11 +15,38 @@ use Dompdf\Dompdf;
 
 $dompdf= new Dompdf();
 
-$dompdf->loadHtml('<h1>Hello World!</h1>');
+$logo = $db->query("SELECT logoPath FROM settings ORDER BY id DESC LIMIT 1;");
+$get_logo = $logo->fetch(PDO::FETCH_ASSOC);
+
+$dompdf->loadHtml('
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Attestation de Licence - Saison 2022/2023</title>
+    <style>
+        img.logo-pdf{
+            display: flex;
+            width: 200px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+<img class="logo-pdf" draggable="false" src="<?= $get_logo["logoPath"] ?>" alt="">
+<div>
+    <h1>A.S. BEUVRY LA FORÊT</h1>
+</div>
+    
+</body>
+</html>');
+
+$dompdf->setPaper('A4', 'portrait');
 
 $dompdf->render();
 
 $dompdf->stream("monasso.org", array("Attachment"=>0));
-
 
 ?>
---
+++ test-pdf.php
@@ -0,0 +1,25 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Attestation de Licence - Saison 2022/2023</title>
+    <style>
+        img.logo-pdf{
+            display: flex;
+            width: 200px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+<img class="logo-pdf" draggable="false" src="/public/logo/logo-asb.png" alt="">
+<div>
+    <h1>A.S. BEUVRY LA FORÊT</h1>
+</div>
+    
+</body>
+</html>
